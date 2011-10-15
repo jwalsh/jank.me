641049624df3026ecfbe9991241935f71af17d5d
Jason Walsh <jason.walsh@memorylane.com>
2011-10-15 14:26:39 -0700

    Using new channel for the timeline integration

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
+alert(1);
+
 window.fbAsyncInit = function() {
   FB.init({
 						appId      : '164543256968230', // App ID
@@ -9,12 +11,31 @@ window.fbAsyncInit = function() {
 					});
 
   // Additional initialization code here
+
+ FB.login(function(response) {
+   if (response.authResponse) {
+     console.log('Welcome!  Fetching your information.... ');
+     FB.api('/me', function(response) {
+       console.log('Good to see you, ' + response.name + '.');
+       FB.logout(function(response) {
+         console.log('Logged out.');
+       });
+     });
+   } else {
+     console.log('User cancelled login or did not fully authorize.');
+   }
+ }, {scope: 'email'});
+
+
 };
 
 // Load the SDK Asynchronously
 (function(d){
-   var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-   js = d.createElement('script'); js.id = id; js.async = true;
+   var js, id = 'facebook-jssdk'; 
+	 if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); 
+	 js.id = id; 
+	 js.async = true;
    js.src = "//connect.facebook.net/en_US/all.js";
    d.getElementsByTagName('head')[0].appendChild(js);
  }(document));
