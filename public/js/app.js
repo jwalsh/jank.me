6d4d20ce16aaafd573f474ffa47b47b1f81720d3
Jason Walsh <jason.walsh@memorylane.com>
2011-10-15 14:32:50 -0700

    Adding additional publish permissions

--- public/js/app.js
+++ public/js/app.js
@@ -12,19 +12,24 @@ window.fbAsyncInit = function() {
 
   // Additional initialization code here
 
- FB.login(function(response) {
-   if (response.authResponse) {
-     console.log('Welcome!  Fetching your information.... ');
-     FB.api('/me', function(response) {
-       console.log('Good to see you, ' + response.name + '.');
-       FB.logout(function(response) {
-         console.log('Logged out.');
-       });
-     });
-   } else {
-     console.log('User cancelled login or did not fully authorize.');
-   }
- }, {scope: 'email'});
+	FB.login(
+		function(response) {
+			if (response.authResponse) {
+				console.log('Welcome!  Fetching your information.... ');
+				FB.api('/me', function(response) {
+								 console.log('Good to see you, ' + response.name + '.');
+								 FB.logout(function(response) {
+														 console.log('Logged out.');
+													 });
+							 });
+			} else {
+				console.log('User cancelled login or did not fully authorize.');
+			}
+		}, 
+		{
+			scope: 'email, publish_actions'
+		}
+	);
 
 
 };
